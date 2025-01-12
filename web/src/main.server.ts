a1a3ef9eeb80f507e808dbe7e9d98b6b3dc17797
Konrad Stępień <kord.stp@gmail.com>
2025-01-12 20:20:18 +0100

    ci(zone.js): remove zone.js and set strict standalone components (#6)

--- web/src/main.server.ts
+++ web/src/main.server.ts
@@ -1,13 +1,13 @@
-import 'zone.js/node';
 import '@angular/platform-server/init';
+
+import { provideServerContext } from '@analogjs/router/server';
+import { ServerContext } from '@analogjs/router/tokens';
 import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { renderApplication } from '@angular/platform-server';
-import { provideServerContext } from '@analogjs/router/server';
-import { ServerContext } from '@analogjs/router/tokens';
 
-import { config } from './app/app.config.server';
 import { AppComponent } from './app/app.component';
+import { config } from './app/app.config.server';
 
 if (import.meta.env.PROD) {
   enableProdMode();
@@ -22,11 +22,9 @@ export default async function render(
   document: string,
   serverContext: ServerContext
 ) {
-  const html = await renderApplication(bootstrap, {
+  return await renderApplication(bootstrap, {
     document,
     url,
     platformProviders: [provideServerContext(serverContext)],
   });
-
-  return html;
 }
